b6c43d20c81a7c81a442dbe0eae2febfbbb79b54
René Lázaro Collado Arteaga <renearteaga261998@gmail.com>
2025-04-14 16:44:34 -0400

    fix tamaños en ShowMeAllTheProducts

--- src/components/ShowMeAllTheProducts.vue
+++ src/components/ShowMeAllTheProducts.vue
@@ -2,11 +2,11 @@
   <div class="my-8 px-4 flex justify-center">
     <a
       to="/categorias"
-      class="inline-flex items-center justify-center gap-1 text-[12px] leading-100% hover:text-gray-900 no-underline py-1 custom-link roboto-condensed-bold"
+      class="inline-flex items-center justify-center gap-1 leading-100% hover:text-gray-900 no-underline py-1 custom-link roboto-condensed-bold"
     >
       <span class="mr-2">Ver todos los productos</span>
       <div class="bg-[#d0cbcb] rounded-full p-1">
-        <img :src="arrowIcon" alt="" class="h-2 w-2 align-middle" />
+        <img :src="arrowIcon" alt="" class="h-3 w-3 align-middle" />
       </div>
     </a>
   </div>
