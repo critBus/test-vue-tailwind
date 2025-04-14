fa207319fd684cf99d11435a3bf38f638ec446d4
René Lázaro Collado Arteaga <renearteaga261998@gmail.com>
2025-04-14 14:24:21 -0400

    porcion cantidades en el carroti en productos recomendados

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,13 +1,49 @@
 <template>
-  <div class="relative inline-block bg-white shadow-md rounded-lg">
-    <div class="p-4 flex flex-row gap-1">
-      <div class="relative inline-block">
+  <div class="relative w-full h-40 inline-block bg-white shadow-md rounded-lg">
+    <div class="p-2 h-full flex flex-row gap-1">
+      <div class="relative w-[40%] inline-block">
         <img class="block w-full h-auto" :src="product.image" alt="Paquete de agua Niagara" />
         <span
-          class="absolute bottom-0 right-0 mb-2 mr-2 z-10 bg-[#FAFAFA] bg-opacity-90 text-[#655F5F] text-[10px] font-medium px-3 py-1 rounded-md"
+          class="absolute bottom-0 right-0 px-3 py-1 z-10 bg-[#FAFAFA] bg-opacity-90 text-[#655F5F] text-[10px] font-medium rounded-md"
         >
           Mercadito TSO
         </span>
+      </div>
+      <div class="flex w-[60%] flex-col gap-2">
+        <span class="font-semibold text-[#4E4949]">{{ product.name }}</span>
+        <div class="flex flex-row gap-2">
+          <span class="font-bold text-[17px]">${{ product.originalPrice }}</span>
+          <span class="text-sm line-through text-red-500">${{ product.price }}</span>
+          <span class="rounded-sm text-[12px] text-[#0B7B69] bg-[#DBFEE3] p-1">Envio Gratis</span>
+        </div>
+        <div class="flex flex-row gap-2">
+          <div class="flex flex-row justify-center items-center">
+            <button
+              @click="quantity -= 1"
+              class="border-2 border-[#E5EAF0] rounded-md px-2 py-1 flex items-center justify-center"
+            >
+              <img src="@/assets/products/icons/less.svg" alt="Tag" class="h-4 w-4" />
+            </button>
+            <div
+              class="w-4 bg-[#CFCECE] border-[#CFCECE] border-t-1 border-b-1 h-[60%] flex justify-center items-center"
+            >
+              <span class="text-[10px] font-bold">{{ quantity }}</span>
+            </div>
+            <button
+              @click="quantity += 1"
+              class="border-2 border-black rounded-md px-2 py-1 flex items-center justify-center"
+            >
+              <img src="@/assets/products/icons/add.svg" alt="Tag" class="h-4 w-4" />
+            </button>
+          </div>
+          <button
+            @click="addToCart"
+            class="bg-[#FCD26D] flex flex-row gap-2 items-center justify-center bg-[#F6F6F6] border-2 border-[#E5EAF0] rounded-md px-2 py-1"
+          >
+            <img src="@/assets/products/icons/shopping-cart.svg" alt="Tag" class="h-4 w-4" />
+            <span class="text-[#624602]">Añadir</span>
+          </button>
+        </div>
       </div>
     </div>
 
