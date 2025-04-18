6da62b533068bcabff0fcafd6da679fc40ea2a9a
René Lázaro Collado Arteaga <renearteaga261998@gmail.com>
2025-04-18 19:12:42 -0400

    HomeView

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,14 @@
 <script setup lang="ts">
-import BestSellingProducts from './components/BestSellingProducts.vue'
-import CategoryNavigation from './components/CategoryNavigation.vue'
 import CopyrightSection from './components/CopyrightSection.vue'
 import FooterSection from './components/FooterSection.vue'
 import HeaderComponent2 from './components/HeaderComponent2.vue'
-import LatestAdditions from './components/LatestAdditions.vue'
-import LinkingYourPurchases from './components/LinkingYourPurchases.vue'
-import RecommendedProducts from './components/RecommendedProducts.vue'
-import SecurePaymentImg from './components/SecurePaymentImg.vue'
-import ShippingCuba from './components/ShippingCuba.vue'
-import Subscriptions from './components/SubscriptionsSection.vue'
-import TopBar from './components/TopBar.vue'
 </script>
 
 <template>
   <HeaderComponent2 />
-  <TopBar />
-  <CategoryNavigation />
-  <RecommendedProducts />
-  <SecurePaymentImg />
-  <BestSellingProducts />
-  <LatestAdditions />
-  <ShippingCuba />
-  <Subscriptions />
-  <LinkingYourPurchases />
+  <RouterView />
   <FooterSection />
   <CopyrightSection />
-  <RouterView />
 </template>
 
 <style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import LatestAdditions from '@/components/LatestAdditions.vue'
+import LinkingYourPurchases from '@/components/LinkingYourPurchases.vue'
+import RecommendedProducts from '@/components/RecommendedProducts.vue'
+import SecurePaymentImg from '@/components/SecurePaymentImg.vue'
+import ShippingCuba from '@/components/ShippingCuba.vue'
+import Subscriptions from '@/components/SubscriptionsSection.vue'
+import TopBar from '@/components/TopBar.vue'
+import BestSellingProducts from '@/components/BestSellingProducts.vue'
+import CategoryNavigation from '@/components/CategoryNavigation.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <TopBar />
+    <CategoryNavigation />
+    <RecommendedProducts />
+    <SecurePaymentImg />
+    <BestSellingProducts />
+    <LatestAdditions />
+    <ShippingCuba />
+    <Subscriptions />
+    <LinkingYourPurchases />
   </main>
 </template>
