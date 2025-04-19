3354298292703c107fc3beb71e110a73364fe745
René Lázaro Collado Arteaga <renearteaga261998@gmail.com>
2025-04-18 21:54:26 -0400

    NavigationMenu

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,12 @@ import CellPhoneNavigation from './components/CellPhoneNavigation.vue'
 import CopyrightSection from './components/CopyrightSection.vue'
 import FooterSection from './components/FooterSection.vue'
 import HeaderComponent2 from './components/HeaderComponent2.vue'
+import NavigationMenu from './components/NavigationMenu.vue'
 </script>
 
 <template>
   <HeaderComponent2 />
+  <NavigationMenu />
   <RouterView />
   <FooterSection />
   <CopyrightSection />
---
+++ src/components/NavigationMenu.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="max-sm:hidden bg-[#E5EAF0] h-12 flex flex-row text-[#7A7474] text-sm roboto gap-5 justify-center items-center"
+  >
+    <button type="button" class="hover:text-black text-[#405774]">Inicio</button>
+    <button type="button" class="hover:text-black">Todos los productos</button>
+    <button type="button" class="hover:text-black">Tiendas</button>
+    <button type="button" class="flex flex-row hover:text-black">
+      <span>Categorías</span>
+      <img :src="IconArrow" alt="" class="w-4 h-4" />
+    </button>
+    <button type="button" class="hover:text-black">Vende con Nosotros</button>
+    <button type="button" class="hover:text-black">Contáctenos</button>
+    <button type="button" class="hover:text-black">Preguntas frecuentes</button>
+  </div>
+</template>
+<script setup lang="ts">
+import IconArrow from '@/assets/navigation_desk/icons/arrow-right.svg'
+</script>
